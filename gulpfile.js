e937ca5e6a65b6912b2339c3a0fb26bc6328ba26
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-02-11 09:00:55 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	minifycss = require('gulp-minify-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps');
+
+
+
+
+gulp.task('sass', function () {
+    gulp.src('./styles/**/*.sass')
+        .pipe(sourcemaps.init())
+            .pipe(sass({
+                indentedSyntax : true,
+                indentType: "tab"
+            }).on('error', sass.logError))
+            .pipe(autoprefixer({
+                browsers : ['last 2 versions'],
+                cascade : false
+            }))
+            .pipe(minifycss())
+        .pipe(sourcemaps.write('./maps'))    
+        .pipe(gulp.dest('./'))
+    });
+
+
+gulp.task('watch', function(){
+	gulp.watch('./styles/**/*.sass', ['sass'])	
+});
+
+gulp.task('default', ['watch', 'sass'], function(){});
